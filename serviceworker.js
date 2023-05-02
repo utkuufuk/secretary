98348d47a91be164b5cffc14de7591069516fe50
Utku Ufuk <utkuufuk@gmail.com>
2023-05-02 20:28:38 +0300

    add fetch event listener

--- serviceworker.js
+++ serviceworker.js
@@ -2,6 +2,11 @@
 self.addEventListener("install", (event) => {
   console.log("Service worker installed");
 });
+
 self.addEventListener("activate", (event) => {
   console.log("Service worker activated");
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log(`URL requested: ${event.request.url}`);
 });
